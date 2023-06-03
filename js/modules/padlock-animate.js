3af3eb21e5c1b97283b4e24725f0eeb5f7def4b2
Xiao Ma <xi.ma@duocuc.cl>
2023-06-02 22:07:51 -0400

    Se agrega la animación al descencriptado de texto

--- js/modules/padlock-animate.js
+++ js/modules/padlock-animate.js
@@ -25,7 +25,26 @@ const handleOpenPadlockWithScroll = () => {
   padlockElement.style.animationDelay = `-${getCurrentStep(padlockData)}s`
 }
 
-export const securityPanelScroll = () => {
-  addEventListener("scroll", handleOpenPadlockWithScroll)
+// Desencriptación  
+
+const dataTextElement = document.getElementById("masked-data-text")
+const keyTextElement = document.getElementById("masked-data-key")
+const messageElement = document.getElementById("message")
+
+const dataTextData = getInitialData(dataTextElement.innerText.length, 150, messageElement)
+const keyTextData = getInitialData(keyTextElement.innerText.length, 150, messageElement)
+
+const handleChangeTextWithScroll = (data, element, steps) => {
+  const currentStep = getCurrentStep(data)
+  const partialText = element.dataset.text.substring(0, currentStep)
+  const partialDots = '·'.repeat(steps - currentStep)
+  element.innerText = partialText + partialDots
 }
 
+export const securityPanelScroll = () => {
+  addEventListener("scroll", () => {
+    handleOpenPadlockWithScroll()
+    handleChangeTextWithScroll(dataTextData, dataTextElement, dataTextElement.innerText.length)
+    handleChangeTextWithScroll(keyTextData, keyTextElement, keyTextElement.innerText.length)
+  })
+}
